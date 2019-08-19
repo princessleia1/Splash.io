689410071be1fdcbe2427e7cb65ebed6b16f03cf
PrincessLeia1 <Kirstene@LAPTOP-5HUS1HH5>
2019-08-19 15:10:13 +1000

    Add project files.

---
+++ Splash.io/Models/SharedDataModelBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Splash.io.Models
+{
+    public abstract class SharedDataModelBase
+    {
+        public string DataFormat { get; set; }
+
+        public string PageTitle { get; set; }
+
+        public string Title { get; set; }
+
+        public SharedDataModelBase()
+        {
+        }
+    }
+}
---
+++ Splash.io/Models/SharedDataWebLinkModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Splash.io.Models
+{
+    public class SharedDataWebLinkModel : SharedDataModelBase
+    {
+        public Uri Uri { get; set; }
+
+        public SharedDataWebLinkModel()
+            : base()
+        {
+        }
+    }
+}
